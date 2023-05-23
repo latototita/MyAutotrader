e18b089e1b2d2fe5415d96caaf8ee2aaa9498522
latototita <85679433+latototita@users.noreply.github.com>
2023-05-23 10:15:18 +0300

    Update s.py

--- s.py
+++ s.py
@@ -185,7 +185,7 @@ async def main():
                     ) and (
                         close_prices[-1] > df['ema'][-1]
                     )and (
-                        adx[-1]> 25) and (sma_50[-1] > sma_200[-1] and sma_50[-2] < sma_200[-2])
+                        adx[-1]> 25)# and (sma_50[-1] > sma_200[-1] and sma_50[-2] < sma_200[-2])
                         
                         
                     sell_signal = (
@@ -201,7 +201,7 @@ async def main():
                     ) and (
                         close_prices[-1] < df['ema'][-1]
                     ) and (
-                        adx[-1]> 25) and (sma_50[-1] < sma_200[-1] and sma_50[-2] > sma_200[-2])
+                        adx[-1]> 25) #and (sma_50[-1] < sma_200[-1] and sma_50[-2] > sma_200[-2])
                     print('B n S teated')
                     # Execute trading orders
                     prices = await connection.get_symbol_price(symbol)
